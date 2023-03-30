1fd74e09b6bcd07a23a0aec3aa73b8e5e617a5a8
MHtazaroie753 <m.h.tazaroie753@gmail.com>
2023-03-30 12:39:59 -0700

    foutrthDay - JSX syntax part one

--- productComponent/src/components/product.jsx
+++ productComponent/src/components/product.jsx
@@ -1,15 +1,42 @@
-import { Component } from 'react'
+import { Component } from "react";
 
 export default class Product extends Component {
   render() {
+    const productName = "iphone 11";
+    const tagList = [
+      <li>tag list item one</li>,
+      <li>tag list item two</li>,
+      <li>tag list item three</li>,
+    ];
+    const dataList = [
+      "data list item one",
+      "data list item two",
+      "data list item three",
+    ];
+    const mapedList = dataList.map((item, i) => <li key={i}>{item}</li>);
     return (
       <>
-      <span className='m-2 text-info'>product name</span>
-      <span className='m-2 badge bg-primary'>2</span>
-      <button className='m-2 btn btn-sm btn-success'>+</button>
-      <button className='m-2 btn btn-sm btn-warning'>-</button>
-      <button className='m-2 btn btn-sm btn-danger '>delete</button>
+        <span className="m-2 text-info">{productName}</span>
+        <span className="m-2 badge bg-primary">{this.format()}</span>
+        <button className="m-2 btn btn-sm btn-success">
+          {this.h6Tag("+")}
+        </button>
+        <button className="m-2 btn btn-sm btn-warning">
+          {this.h6Tag("-")}
+        </button>
+        <button className="m-2 btn btn-sm btn-danger ">delete</button>
+        <ul>
+          {tagList}
+          ---------
+          {mapedList}
+        </ul>
       </>
-    )
+    );
+  }
+  format() {
+    return 2 + 2;
+  }
+  h6Tag(value) {
+    return <h6>{value}</h6>;
   }
 }
