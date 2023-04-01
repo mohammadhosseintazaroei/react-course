2a1c125d287ce2e6a86bef4ed9bde11de775fd4f
MHtazaroie753 <m.h.tazaroie753@gmail.com>
2023-04-01 08:03:41 -0700

    fifthDay - Events

--- productComponent/src/components/product.jsx
+++ productComponent/src/components/product.jsx
@@ -1,21 +1,41 @@
 import { Component } from "react";
-import "./product.css"
 export default class Product extends Component {
   count = 0;
-  imageUrl = "https://picsum.photos/200"
   render() {
     return (
       <>
         <span className="m-2 text-info">product name</span>
         <span className="m-2 badge bg-primary">{this.format()}</span>
-        <button className="m-2 btn btn-sm btn-success">+</button>
-        <button className="m-2 btn btn-sm btn-warning">-</button>
-        <button className="m-2 btn btn-sm btn-danger ">delete</button>
-        <img src={this.imageUrl} /*style={{borderRadius:"50%"}} */ alt="" />
+        <button
+          onClick={this.handelIncreamente.bind(this)}
+          className="m-2 btn btn-sm btn-success"
+        >
+          +
+        </button>
+        <button
+          onClick={this.handelDecreamente}
+          className="m-2 btn btn-sm btn-warning"
+        >
+          -
+        </button>
+        <button
+          onClick={() => this.handelDelete(1)}
+          className="m-2 btn btn-sm btn-danger "
+        >
+          delete
+        </button>
       </>
     );
   }
-
+  handelIncreamente() {
+    console.log("increamente", this);
+  }
+  handelDecreamente = () => {
+    console.log("decreamente", this);
+  };
+  handelDelete(itemNumber) {
+    console.log("delete", itemNumber);
+  }
   format() {
     if (this.count == 0) {
       return "zero";
