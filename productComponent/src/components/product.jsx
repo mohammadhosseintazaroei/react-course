6dccef9e43d406f9e76a98d47bef53cfbb0d8f3e
MHtazaroie753 <m.h.tazaroie753@gmail.com>
2023-04-01 07:41:15 -0700

    fifthDay - JSX syntax part three

--- productComponent/src/components/product.jsx
+++ productComponent/src/components/product.jsx
@@ -1,21 +1,17 @@
 import { Component } from "react";
-
+import "./product.css"
 export default class Product extends Component {
   count = 0;
+  imageUrl = "https://picsum.photos/200"
   render() {
     return (
       <>
         <span className="m-2 text-info">product name</span>
         <span className="m-2 badge bg-primary">{this.format()}</span>
-        <span className="m-2 badge bg-primary">
-          {this.count === 0 ? "zero" : this.count}
-        </span>
-        <span className="m-2 badge bg-primary">
-          {this.count !== 0 && this.count}
-        </span>
         <button className="m-2 btn btn-sm btn-success">+</button>
         <button className="m-2 btn btn-sm btn-warning">-</button>
         <button className="m-2 btn btn-sm btn-danger ">delete</button>
+        <img src={this.imageUrl} /*style={{borderRadius:"50%"}} */ alt="" />
       </>
     );
   }
